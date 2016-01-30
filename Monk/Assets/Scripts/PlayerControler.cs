c6e67a66917dce95e07c3121b25f2f670924394e
Rednell <rafa.santanas@gmail.com>
2016-01-30 12:15:14 -0300

    Player Changes
    Changes in player movement.

--- Monk/Assets/Scripts/PlayerControler.cs
+++ Monk/Assets/Scripts/PlayerControler.cs
@@ -3,37 +3,65 @@ using System.Collections;
 
 public class PlayerControler : MonoBehaviour {
 	public LayerMask layer;
+    public float timeMovement;
+    float TimeStarted;
 
-	// Use this for initialization
-	void Start () {
-		RaycastHit hit;
+    Vector3 positionTarget;
+
+    //states of the player
+    private enum PlayerState { iddle, moving };
+    private PlayerState state;
+
+    //used for movement
+    private Vector3 startPosition;
+    private Vector3 destinyPosition;
+
+    // Use this for initialization
+    void Start () {
+        RaycastHit hit;
 		if (Physics.Raycast(new Ray(transform.position, Vector3.down* 0.75f), out hit, 1f, layer.value)) {
 			if (hit.collider.GetComponent<MazeFloor>())
 				transform.parent = hit.collider.transform;
 			else
 				throw new System.Exception("Place the Player over some MazeFloor");
 		}
-	}
+
+        positionTarget = transform.localPosition;
+    }
 	
 	// Update is called once per frame
 	void Update () {
+
+        //transform.localPosition = Vector3.Lerp(transform.localPosition, positionTarget, moveSpeed * Time.deltaTime);
+
 		if (Time.timeScale < 0.9f) return;
-			
-		if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-			transform.localRotation *= Quaternion.AngleAxis(-90, Vector3.up);
-		}
+        if (state.Equals(PlayerState.iddle)){
+            if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+                transform.localRotation *= Quaternion.AngleAxis(-90, Vector3.up);
+            }
 
-		if (Input.GetKeyDown(KeyCode.RightArrow)) {
-			transform.localRotation *= Quaternion.AngleAxis(90, Vector3.up);
-		}
+            if (Input.GetKeyDown(KeyCode.RightArrow)) {
+                transform.localRotation *= Quaternion.AngleAxis(90, Vector3.up);
+            }
 
-		if (Input.GetKeyDown(KeyCode.UpArrow)) {
-			MoveIfCan(transform.forward);
-		}
+            if (Input.GetKey(KeyCode.UpArrow)) {
+                MoveIfCan(transform.forward);
+            }
 
-		if (Input.GetKeyDown(KeyCode.DownArrow)) {
-			MoveIfCan(-transform.forward);
-		}
+            if (Input.GetKey(KeyCode.DownArrow)) {
+                MoveIfCan(-transform.forward);
+            }
+        } else if (state.Equals(PlayerState.moving)) {
+            float actualTime = Time.time;
+            float timePassedBy = actualTime - TimeStarted;
+            float progress = timePassedBy / timeMovement;
+            transform.
+            transform.localPosition = Vector3.Lerp(startPosition, destinyPosition, progress);
+            if (timePassedBy >= timeMovement)
+            {
+                state = PlayerState.iddle;
+            }
+        }
 	}
 
 	void MoveIfCan (Vector3 delta) {
@@ -52,7 +80,10 @@ public class PlayerControler : MonoBehaviour {
 			return; // cant move in float air
 		}
 
-		transform.localPosition += delta; 
+        startPosition = transform.localPosition;
+        destinyPosition = transform.localPosition + delta;
+        this.TimeStarted = Time.time;
+        state = PlayerState.moving;
 	}
 
 	void OnTriggerEnter (Collider other) {
